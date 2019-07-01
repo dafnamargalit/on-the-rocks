36da699177695da8f0038f92007d0d7d1d4d7195
Dafna <dama7429@colorado.edu>
2019-07-01 15:39:49 +0300

    router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Home from '../Home';
+import About from '../About';
+import Contact from '../Contact';
+import Gigs from '../Gigs';
+import MTM from '../MTM';
 
 import './App.css';
 
@@ -12,7 +16,10 @@ class App extends Component {
     <Router>
       <div className='App'>
       <Route exact path='/' component={Home}/>
-     
+      <Route exact path='/about' component={About}/>
+      <Route exact path='/meet-the-members' component={MTM}/>
+      <Route exact path='/gigs' component={Gigs}/>
+      <Route exact path='/contact' component={Contact}/>
       </div>
     </Router>
   );
